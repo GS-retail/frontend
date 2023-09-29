d18c8a94dde6dc7ec8b44747fd7b733b13303d50
toothlessdev <toothless042@gmail.com>
2023-09-29 23:15:07 +0900

    feat: NavMenu, Header 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./store/store";
-import { Nav } from "./components/Nav";
+import { Header } from "@/components/Header";
 
 import HomePage from "@/pages/HomePage";
 
 export default function App() {
     return (
         <Provider store={store}>
-            <Nav />
+            <Header />
 
             <Routes>
                 <Route path="/" element={<HomePage />} />
