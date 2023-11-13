6281cb78fd4e8cd002896f1dafecc64889064ec4
toothlessdev <toothless042@gmail.com>
2023-11-13 22:43:09 +0900

    feat: InputContainer 컴포넌트 구현

--- src/components/interface/Form.tsx
+++ src/components/interface/Form.tsx
@@ -1,9 +1,12 @@
+import { IForm, IInput, IInputContainer, ILabel } from "./types";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Form.module.scss";
+import { Button } from "./Button";
 
-export const Search = () => {
+export const Search = (): JSX.Element => {
     return (
         <div className={styles.search_wrapper}>
             <input type="text" placeholder="검색어를 입력하세요"></input>
@@ -14,7 +17,7 @@ export const Search = () => {
     );
 };
 
-export const Form = ({ children, width, height, ...rest }) => {
+export const Form = ({ children, width, height, ...rest }: IForm) => {
     return (
         <form style={{ width: width, height: height }} {...rest}>
             {children}
@@ -22,15 +25,21 @@ export const Form = ({ children, width, height, ...rest }) => {
     );
 };
 
-export const Input = ({ width, height, ...rest }) => {
+export const Input = ({ width, height, ...rest }: IInput) => {
     return <input style={{ width: width, height: height }} className={styles.input} {...rest}></input>;
 };
 
-export const InputContainer = ({ label, type, btnRequired, btnLabel, onClick }) => {
-    return <div className={styles.input_container}></div>;
+export const InputContainer = ({ label, type, placeholder, btnRequired, btnLabel, onClick, ...rest }: IInputContainer) => {
+    return (
+        <div className={styles.input_container}>
+            <p>{label}</p>
+            <Input type={type} placeholder={placeholder} value={type === "button" ? placeholder : null} />
+            {btnRequired && <Button type="primary-stroke">{btnLabel}</Button>}
+        </div>
+    );
 };
 
-export const Label = ({ children, ...rest }) => {
+export const Label = ({ children, ...rest }: ILabel) => {
     return (
         <label className={styles.label} {...rest}>
             {children}
