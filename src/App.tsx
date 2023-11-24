9616c91e759e5c9bc547293b0bfc670d76c68762
toothlessdev <toothless042@gmail.com>
2023-11-24 21:29:32 +0900

    feat: NavLayout 으로 라우트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./store/store";
-import { Header } from "./components/Header";
 
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import { NavLayout } from "./layouts/NavLayout";
 
 export default function App() {
     return (
         <Provider store={store}>
-            <Header />
-
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/auth/signin" element={<AuthPage.SignIn />} />
-                <Route path="/auth/signup" element={<AuthPage.SignUp />} />
+                <Route path="/" element={<NavLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/auth/signin" element={<AuthPage.SignIn />} />
+                    <Route path="/auth/signup" element={<AuthPage.SignUp />} />
+                </Route>
             </Routes>
         </Provider>
     );
