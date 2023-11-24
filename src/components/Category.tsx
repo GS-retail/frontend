cb897aba3b593147057c0814a993f8b346115249
toothlessdev <toothless042@gmail.com>
2023-11-24 21:39:41 +0900

    chore: Category className 수정

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,19 +4,19 @@ import { ICategoryContainer, ICategoryItem, ICategoryItems } from "./types";
 export const Category = {
     Container: ({ children }: ICategoryContainer): JSX.Element => {
         return (
-            <div className={styles.wrapper}>
-                <div className={styles.container}>{children}</div>
+            <div className={styles.category_wrapper}>
+                <div className={styles.category_container}>{children}</div>
             </div>
         );
     },
 
     Items: ({ children }: ICategoryItems) => {
-        return <div className={styles.items}>{children}</div>;
+        return <div className={styles.category_items}>{children}</div>;
     },
 
     Item: ({ imgSrc, text }: ICategoryItem): JSX.Element => {
         return (
-            <div className={styles.item}>
+            <div className={styles.category_item}>
                 <img src={imgSrc}></img>
                 <p>{text}</p>
             </div>
